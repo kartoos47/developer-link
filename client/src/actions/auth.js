647ce59a9512ffca38ef265b884c96d75b8d8163
kartikey <kartikeygarg92@gmail.com>
2019-07-20 19:57:14 +0530

    add profile component & get current user profile action and reducer, create profile form

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from "./types";
 
 import axios from "axios";
@@ -82,5 +83,6 @@ export const login = (email, password) => async dispatch => {
 
 //Logout / Clear Profile
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
